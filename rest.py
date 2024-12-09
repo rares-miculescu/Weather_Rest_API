a617ee9acabafb5e4ae5d119022decc41d91ecde
Rares Miculescu <miculescur@gmail.com>
2024-12-09 13:33:25 +0200

    a mai ramas de facut GET la temp

--- rest.py
+++ rest.py
@@ -9,8 +9,6 @@ import os
 
 app = Flask(__name__)
 
-
-
 uri = "mongodb://root:toor@localhost:27017/"
 client = MongoClient(uri)
 db = client['tema2']
@@ -54,7 +52,7 @@ def get_countries():
         countries.append(c)
     return countries, 200
 
-@app.route('/api/countries/<string:id>', methods = ["PUT"])
+@app.route('/api/countries/<id>', methods = ["PUT"])
 def modify_country(id):
 
     ok = False
@@ -79,14 +77,14 @@ def modify_country(id):
         not (isinstance(payload['lon'], int) or isinstance(payload['lon'], float))):
         return jsonify({'status':'wrong types'}), 400
     if str(payload['id']) != id:
-        return jsonify({'status':'wrong id'}), 400
+        return jsonify({'status':'wrong id'}), 409
     
     filter = {'_id': ObjectId(id)} 
     db['country'].update_one(filter, {'$set': {'nume': payload['nume'], 'lat': payload['lat'], 'lon': payload['lon']}})
 
     return jsonify({'status':'ok'}), 200
 
-@app.route('/api/countries/<string:id>', methods = ["DELETE"])
+@app.route('/api/countries/<id>', methods = ["DELETE"])
 def rm_country(id):
     ok = False
     for country in db['country'].find():
@@ -148,7 +146,7 @@ def get_cities():
         cities.append(c)
     return cities, 200
 
-@app.route('/api/cities/country/<string:id>', methods = ["GET"])
+@app.route('/api/cities/country/<id>', methods = ["GET"])
 def get_cities_country(id):
 
     ok = False
@@ -171,7 +169,7 @@ def get_cities_country(id):
             cities.append(c)
     return cities, 200
 
-@app.route('/api/cities/<string:id>', methods = ["PUT"])
+@app.route('/api/cities/<id>', methods = ["PUT"])
 def change_city(id):
 
     ok = False
@@ -196,7 +194,7 @@ def change_city(id):
         not (isinstance(payload['lon'], int) or isinstance(payload['lon'], float))):
         return jsonify({'status':'wrong types'}), 400
     if str(payload['id']) != id:
-        return jsonify({'status':'wrong id'}), 400
+        return jsonify({'status':'wrong id'}), 409
     
     ok = False
     for country in db['country'].find():
@@ -211,13 +209,64 @@ def change_city(id):
 
     return jsonify({'status':'ok'}), 200
 
-# @app.route('/api/cities/<int:id>', methods = ["DELETE"])
-# def rm_city():
-#     return jsonify({'status':'ok rm city'}), 200
+@app.route('/api/cities/<id>', methods = ["DELETE"])
+def rm_city(id):
 
-# @app.route('/api/temperatures', methods = ["POST"])
-# def add_temp():
-#     return jsonify({'status':'ok add temp'}), 200
+    ok = False
+    for city in db['city'].find():
+        if str(city['_id']) == id:
+            ok = True
+            break
+    if not ok:
+        return jsonify({'status':'city not found'}), 404
+    
+    filter = {'_id': ObjectId(id)}
+    db['city'].delete_one(filter)
+
+    return jsonify({'status':'ok'}), 200
+
+@app.route('/api/temperatures', methods = ["POST"])
+def add_temp():
+
+    payload = request.get_json(silent=True)
+    if len(payload) != 2:
+        return jsonify({'status':'wrong number of arguments'}), 400
+    if ('idOras' not in payload or
+        'valoare' not in payload):
+        return jsonify({'status':'wrong arguments'}), 400
+    if (not (isinstance(payload['valoare'], float) or isinstance(payload['valoare'], int))):
+        return jsonify({'status':'wrong types'}), 400
+
+    id = payload['idOras']
+    ok = False
+    for city in db['city'].find():
+        if str(city['_id']) == id:
+            ok = True
+            break
+    if not ok:
+        return jsonify({'status':'city not found'}), 404
+    
+    timestamp = datetime.datetime.now()
+    result = db['temperature'].insert_one({"idOras": payload['idOras'], "valoare": payload['valoare'], "timestamp": timestamp})
+    return jsonify({'id': str(result.inserted_id)}), 201
+
+# def get_all_temps():
+#     temperatures = []
+#     for temp in db['temperature'].find():
+#         temp['_id'] = str(temp['_id'])
+#         t = {"id":str(temp['_id']),
+#             "idOras":str(temp['idOras']),
+#             "valoare":temp['valoare'],
+#             "timestamp":temp['timestamp']}
+#         temperatures.append(t)
+#     return temperatures
+
+# def get_temp(type: str, lat: float, lon: float, date_from: datetime, date_until: datetime):
+    
+#     if type is None:
+            
+
+        
 
 # @app.route('/api/temperatures', methods = ["GET"])
 # def get_precise_temp():
@@ -225,6 +274,11 @@ def change_city(id):
 #     lon = request.args.get('lon', type=float)
 #     date_from = request.args.get('from', type=lambda d: datetime.strptime(d, '%Y-%m-%d'))
 #     date_until = request.args.get('until', type=lambda d: datetime.strptime(d, '%Y-%m-%d'))
+    
+    
+#     res = get_temp(None, lat, lon, date_from, date_until)
+#     if res == 0:
+#         return jsonify({'status':'no data'}), 404
 #     return jsonify({'status':'ok get precise temp'}), 200
 
 # @app.route('/api/temperatures/<int:id>', methods = ["GET"])
@@ -239,13 +293,60 @@ def change_city(id):
 #     date_until = request.args.get('until', type=lambda d: datetime.strptime(d, '%Y-%m-%d'))
 #     return jsonify({'status':'ok get country interval temp'}), 200
 
-# @app.route('/api/temperatures/<int:id>', methods = ["PUT"])
-# def change_temp(id):
-#     return jsonify({'status':'ok change temp'}), 200
+@app.route('/api/temperatures/<id>', methods = ["PUT"])
+def change_temp(id):
 
-# @app.route('/api/temperatures', methods = ["DELETE"])
-# def delete_temp():
-#     return jsonify({'status':'ok delete temp'}), 200
+    ok = False
+    for temp in db['temperature'].find():
+        if str(temp['_id']) == id:
+            ok = True
+            time_id = temp['timestamp']
+            break
+    if not ok:
+        return jsonify({'status':'temp not found'}), 404
+
+    payload = request.get_json(silent=True)
+    if len(payload) != 3:
+        return jsonify({'status':'wrong number of arguments'}), 400
+    if ('id' not in payload or
+        'idOras' not in payload or
+        'valoare' not in payload):
+        return jsonify({'status':'wrong arguments'}), 400
+    if (not (isinstance(payload['valoare'], float) or isinstance(payload['valoare'], int))):
+        return jsonify({'status':'wrong types'}), 400
+    if str(payload['id']) != id:
+        return jsonify({'status':'wrong id'}), 409
+
+    idc = payload['idOras'] 
+    ok = False
+    for city in db['city'].find():
+        if str(city['_id']) == idc:
+            ok = True
+            break
+    if not ok:
+        return jsonify({'status':'city not found'}), 404
+
+    filter = {'_id': ObjectId(id)}
+    db['temperature'].update_one(filter, {'$set': {'idOras': payload['idOras'], 'valoare': payload['valoare']}})
+
+    return jsonify({'status':'ok'}), 200
+
+@app.route('/api/temperatures/<id>', methods = ["DELETE"])
+def delete_temp(id):
+    ok = False
+    for temp in db['temperature'].find():
+        if str(temp['_id']) == id:
+            ok = True
+            break
+    if not ok:
+        return jsonify({'status':'city not found'}), 404
+    
+    filter = {'_id': ObjectId(id)}
+    db['city'].delete_one(filter)
+
+    return jsonify({'status':'ok'}), 200
+
+
 
 def init_db():
 
@@ -261,11 +362,10 @@ def init_db():
     city_collection = db['city']
     city_collection.create_index([('id', ASCENDING)], unique=False)
 
-    # if 'temperature' not in collections:
-    #     db.create_collection('temperature')
-    
-    # temperature_collection = db['temperature']
-    # temperature_collection.create_index([('id', ASCENDING)], unique=True)
+    if 'temperature' not in collections:
+        db.create_collection('temperature')    
+    temperature_collection = db['temperature']
+    temperature_collection.create_index([('id', ASCENDING)], unique=False)
 
 if __name__ == '__main__':
     init_db()
